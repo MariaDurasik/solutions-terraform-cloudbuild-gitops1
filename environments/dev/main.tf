8bab9e0505c9f47ab4d1d51d5ed1a56f565a94a9
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-26 12:28:52 +0200

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -39,25 +39,36 @@ module "firewall" {
   subnet  = "${module.vpc.subnet}"
 }
   
-  resource "google_sql_database_instance" "master" {
-  name = "master-instance-${random_id.db_name_suffix.hex}"
+module "mysql" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "github.com/gruntwork-io/terraform-google-sql.git//modules/cloud-sql?ref=v0.2.0"
+  source = "./modules/cloud-sql"
 
-  settings {
-    tier = "db-f1-micro"
+  project = var.project
+  region  = var.region
+  name    = local.instance_name
+  db_name = var.db_name
+
+  engine       = var.mysql_version
+  machine_type = var.machine_type
+
+  # These together will construct the master_user privileges, i.e.
+  # 'master_user_name'@'master_user_host' IDENTIFIED BY 'master_user_password'.
+  # These should typically be set as the environment variable TF_VAR_master_user_password, etc.
+  # so you don't check these into source control."
+  master_user_password = var.master_user_password
+
+  master_user_name = var.master_user_name
+  master_user_host = "%"
+
+  # Pass the private network link to the module
+  private_network = google_compute_network.private_network.self_link
+
+  # Wait for the vpc connection to complete
+  dependencies = [google_service_networking_connection.private_vpc_connection.network]
+
+  custom_labels = {
+    test-id = "mysql-private-ip-example"
   }
-}
-
-resource "google_sql_database" "database" {
-  name     = "my-database"
-  instance = google_sql_database_instance.master.name
-}
-
-resource "google_sql_user" "users" {
-  name     = "user"
-  instance = google_sql_database_instance.master.name
-  password = "Eey3ar8fz343uciy"
-}
-
-
-data "google_cloud_run_locations" "available" {
 }
