14509145ae88d94a138f7a63162cfa18afdb6b8c
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-28 14:15:45 +0200

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,22 +21,6 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
 }
 module "sql-db" {
   source  = "GoogleCloudPlatform/sql-db/google//modules/mysql"
