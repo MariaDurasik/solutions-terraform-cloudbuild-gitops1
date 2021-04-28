18bfc30cfb9b22ce7549fb83f5cf1a669d5125b6
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-28 12:25:10 +0200

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -59,13 +59,6 @@ resource "random_id" "db_name_suffix" {
   byte_length = 4
 }
 
-resource "google_sql_database_instance" "master" {
-  name = "master-instance-${random_id.db_name_suffix.hex}"
-
-  settings {
-    tier = "db-f1-micro"
-  }
-}
 
 resource "google_sql_database" "database" {
   name     = "my-database"
