aede9dfc6eeaa5e3c9d53d128442379f57232c0f
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-28 12:21:42 +0200

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -39,6 +39,32 @@ module "firewall" {
   subnet  = "${module.vpc.subnet}"
 }
   
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
 resource "google_sql_database_instance" "master" {
   name = "master-instance-${random_id.db_name_suffix.hex}"
 
