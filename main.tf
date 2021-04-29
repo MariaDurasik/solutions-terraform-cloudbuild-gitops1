3c924ef677f03348709e3941476b4fe9978cb902
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-29 14:06:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,23 +33,6 @@ module "gcloud" {
 }
 
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
-
 resource "random_id" "db_name_suffix" {
 byte_length = 4
 }
