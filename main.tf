5bd9661dec6c4f0370c595f3ba79c1352338b945
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-29 15:07:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,48 +36,8 @@ resource "random_id" "db_name_suffix" {
 byte_length = 4
 }
   
-resource "google_compute_network" "private_network" {
-  provider = google-beta
-
-  name = "private-network"
-}
-
-resource "google_compute_global_address" "private_ip_address" {
-  provider = google-beta
-
-  name          = "private-ip-address"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = google_compute_network.private_network.id
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-  provider = google-beta
-
-  network                 = google_compute_network.private_network.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-}
-
-resource "google_sql_database_instance" "instance" {
-  provider = google-beta
-
-  name   = "private-instance-${random_id.db_name_suffix.hex}"
-  region = "us-central1"
-
-  depends_on = [google_service_networking_connection.private_vpc_connection]
-
-  settings {
-    tier = "db-f1-micro"
-    ip_configuration {
-      ipv4_enabled    = false
-      private_network = google_compute_network.private_network.id
-    }
-  }
-}
-
-provider "google-beta" {
-  region = "us-central1"
-  zone   = "us-central1-a"
+module "sql-db_mysql" {
+  source  = "GoogleCloudPlatform/sql-db/google//modules/mysql"
+  version = "5.0.0"
+  # insert the 5 required variables here
 }
