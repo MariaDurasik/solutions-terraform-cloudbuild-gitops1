88df6d4e878ee719fd6b0de7d1e5a3dec61578d4
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-29 13:50:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,18 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
+  resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
 
 resource "random_id" "db_name_suffix" {
 byte_length = 4
