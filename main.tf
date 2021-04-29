00de859edfad29237907aa6f78764c6f6c47a9bc
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-04-29 11:16:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-locals {
-  "env" = "dev"
-}
 
 provider "google" {
   project = "${var.project}"
+  region  = "europe-central2"
+  zone    = "europe-central2-a"
 }
 
 resource "google_sql_database" "database" {
