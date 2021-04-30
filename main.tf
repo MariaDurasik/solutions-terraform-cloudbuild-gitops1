2103ec078907fa90b45b844f3f8c3b25e7e96885
MariaDurasik <maria.durasik@gmail.com>
2021-04-30 14:09:39 +0200

    jfjf

--- main.tf
+++ main.tf
@@ -14,18 +14,47 @@
  * limitations under the License.
  */
 
-provider "google-beta" {
-  version = ">= 3.1.0, <4.0.0"
-  region  = var.region
+provider "google" {
+  version = "~> 3.22"
 }
 
-module "mssql" {
-  source               = "../../modules/mssql"
-  name                 = var.name
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+resource "random_id" "name" {
+  byte_length = 2
+}
+
+module "mysql-db" {
+  source               = "../../modules/mysql"
+  name                 = var.db_name
   random_instance_name = true
+  database_version     = "MYSQL_5_6"
   project_id           = var.project_id
-  user_name            = "simpleuser"
-  user_password        = "foobar"
+  zone                 = "us-central1-c"
+  region               = "us-central1"
+  tier                 = "db-n1-standard-1"
 
   deletion_protection = false
+
+  ip_configuration = {
+    ipv4_enabled        = true
+    private_network     = null
+    require_ssl         = true
+    authorized_networks = var.authorized_networks
+  }
+
+
+  database_flags = [
+    {
+      name  = "log_bin_trust_function_creators"
+      value = "on"
+    },
+  ]
 }
+
--- variables.tf
+++ variables.tf
@@ -15,17 +15,21 @@
  */
 
 variable "project_id" {
+  description = "The ID of the project in which resources will be provisioned."
   type        = string
-  description = "The project to run tests against"
 }
 
-variable "name" {
-  type        = string
-  description = "The name for Cloud SQL instance"
-  default     = "tf-mssql-public"
+variable "db_name" {
+  description = "The name of the SQL Database instance"
+  default     = "example-mysql-public"
 }
 
-variable "region" {
-  default = "us-central1"
-  type    = string
+variable "authorized_networks" {
+  default = [{
+    name  = "sample-gcp-health-checkers-range"
+    value = "130.211.0.0/28"
+  }]
+  type        = list(map(string))
+  description = "List of mapped public networks authorized to access to the instances. Default - short range of GCP health-checkers IPs"
 }
+
