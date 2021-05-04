48350f11403592531776ad9d8bf429cb59d9fe08
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 09:00:49 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -44,3 +44,12 @@ output "private_ip_address" {
   value       = module.mysql-db.private_ip_address
 }
 
+output "instances_self_links" {
+  description = "List of self-links for compute instances"
+  value       = module.compute_instance.instances_self_links
+}
+
+output "available_zones" {
+  description = "List of available zones in region"
+  value       = module.compute_instance.available_zones
+}
