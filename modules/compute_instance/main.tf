0b7173fa32aa2194acc98c0ebdd2abf872274d30
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 11:58:02 +0200

    Update main.tf

--- modules/compute_instance/main.tf
+++ modules/compute_instance/main.tf
@@ -30,7 +30,6 @@ locals {
 ###############
 resource "google_service_account" "default" {
   project      = "secound-312107"
-  account_id   = "service-account-id"
   display_name = "Service Account"
 }
 
