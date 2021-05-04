e6006cd56ce67ad2ba175dd4bc281650a58a6a8e
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 08:55:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,6 +29,29 @@ provider "random" {
 resource "random_id" "name" {
   byte_length = 2
 }
+   
+module "instance_template" {
+  source          = "./modules/instance_template"
+  region          = var.region
+  project_id      = var.project_id
+  subnetwork      = var.subnetwork
+  service_account = var.service_account
+}
+
+module "compute_instance" {
+  source            = "./modules/compute_instance"
+  region            = var.region
+  zone              = var.zone
+  subnetwork        = var.subnetwork
+  num_instances     = var.num_instances
+  hostname          = "instance-simple"
+  instance_template = module.instance_template.self_link
+  access_config = [{
+    nat_ip       = var.nat_ip
+    network_tier = var.network_tier
+  }, ]
+}
+
 
 module "mysql-db" {
   source               = "./modules/mysql"
