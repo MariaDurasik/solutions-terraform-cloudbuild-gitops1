fc520f7848a8d064919b582720a82524668a6bfb
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 13:40:10 +0200

    Update variables.tf

--- modules/instance_template/variables.tf
+++ modules/instance_template/variables.tf
@@ -173,7 +173,6 @@ variable "service_account" {
   })
   default = {
      email  = 897416692031
-     scopes = [cloud-platform]
     }
   description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template.html#service_account."
 }
