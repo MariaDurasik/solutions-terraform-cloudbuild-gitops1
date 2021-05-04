508f5ab7850050d3abf7bf411daa33f55083f21f
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 11:42:52 +0200

    Update main.tf

--- modules/instance_template/main.tf
+++ modules/instance_template/main.tf
@@ -17,7 +17,8 @@
 ###############
 # Data Sources
 ###############
-   resource "google_service_account" "default" {
+resource "google_service_account" "default" {
+  project      = "secound-312107"
   account_id   = "service-account-id"
   display_name = "Service Account"
 }
