a250077558087fa259428ad5a2c489e0b8753500
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 09:42:06 +0200

    Update main.tf

--- modules/compute_instance/main.tf
+++ modules/compute_instance/main.tf
@@ -37,10 +37,7 @@ data "google_compute_zones" "available" {
 #############
 # Instances
 #############
-resource "google_service_account" "default" {
-  account_id   = "service_account_id"
-  display_name = "Service Account"
-}
+
 
 resource "google_compute_instance_from_template" "compute_instance" {
   provider = google
