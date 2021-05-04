648b319ec40a3136379bd0b8c458b6d08209d012
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-04 09:36:29 +0200

    Update main.tf

--- modules/compute_instance/main.tf
+++ modules/compute_instance/main.tf
@@ -61,10 +61,3 @@ resource "google_compute_instance_from_template" "compute_instance" {
 
   source_instance_template = var.instance_template
 }
-
-   service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-
