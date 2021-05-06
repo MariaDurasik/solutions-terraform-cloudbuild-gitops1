5c2e2e19ec105760f6aa258b8a67df19cf57f0de
MariaDurasik <83211266+MariaDurasik@users.noreply.github.com>
2021-05-06 13:38:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "google_compute_network" "network" {
 
 resource "google_compute_subnetwork" "subnet" {
   project                  = var.project
-  name                     = "test-subnet-iap"
+  name                     = "test-subnet-iap1"
   region                   = var.region
   ip_cidr_range            = "10.127.0.0/20"
   network                  = google_compute_network.network.self_link
